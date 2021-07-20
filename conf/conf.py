fc891d403f032c9616767fa79ce0c8ddc6d24d89
MarieDeSmedt <desmedtmarie@outlook.com>
2021-07-20 16:22:21 +0200

    first commit

---
+++ conf/conf.py
@@ -0,0 +1,5 @@
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "d9a0be78f2791778dddbd288f0935e2826e9ae7678b345a65e36b22e3f0be487"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
---
+++ utils/user_utils.py
@@ -0,0 +1,53 @@
+from models.user import UserInDB
+from fastapi import Depends
+from auth_utils import oauth2_scheme, verify_password
+from fastapi import Depends, FastAPI, HTTPException, status
+from conf.conf import SECRET_KEY, ALGORITHM
+from jose import JWTError, jwt
+from database.fake_users_db import fake_users_db
+from models.user import User
+from models.token import TokenData
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+def authenticate_user(fake_db, username: str, password: str):
+
+    user = get_user(fake_db, username)
+
+    if not user:
+
+        return False
+
+    if not verify_password(password, user.hashed_password):
+
+        return False
+
+    return user
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
