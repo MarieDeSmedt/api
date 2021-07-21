cb9582d6dd6685c9ca85685bed911f1c15b230f6
MarieDeSmedt <desmedtmarie@outlook.com>
2021-07-21 11:14:20 +0200

    commit

--- app/app.py
+++ app/app.py
@@ -24,14 +24,11 @@ app = FastAPI()
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"sub": user.username, "scopes": form_data.scopes},
+        expires_delta=access_token_expires,
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
@@ -49,7 +46,7 @@ async def read_own_items(current_user: User = Depends(get_current_active_user)):
 
 @app.get("/{input}")
 def predict(input: str, current_user: User = Depends(get_current_active_user)):
-    if current_user:
+    if login_for_access_token() :
         tfidf, model = pickle.load(open('model.bin', 'rb'))
         predictions = model.predict(tfidf.transform([input]))
         label = predictions[0]
